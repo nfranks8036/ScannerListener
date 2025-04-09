c5b0a8ba7fde84170a5febe66766cee2eaf90386
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-04-09 12:19:06 -0400

    Starting from scratch with an RTL-SDR

--- src/main/java/net/noahf/scanner/Main.java
+++ src/main/java/net/noahf/scanner/Main.java
@@ -1,28 +1,10 @@
 package net.noahf.scanner;
 
-import net.noahf.scanner.audio.AudioListener;
-
 public class Main {
-
-    public static final int BUFFER_SIZE = 16384; // for ftt analysis
 
     public static void main(String[] args) {
         try {
-            AudioListener audioListener = new AudioListener();
 
-            audioListener.setTick((frame) -> {
-//                System.out.println("SOUND DETECTED: " + frame.getVolume() + " dB, " + frame.getFrequency() + " MHz, " + (System.currentTimeMillis() - frame.getLastSoundDetectionTime()) + "ms");
-                if (!frame.hasSoundRecently()) {
-                    if (frame.getRecorder().isRecording()) {
-                        frame.getRecorder().stopRecording();
-                    }
-                    return;
-                }
-
-                if (!frame.getRecorder().isRecording()) {
-                    frame.getRecorder().startRecording();
-                }
-            });
         } catch (Exception exception) {
             throw new RuntimeException("ScannerListener failed to execute: " + exception, exception);
         }
